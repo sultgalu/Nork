9c409abbb0fd5a46c1c8eb5350d1673fd87ef07b
sultgalu <komlosi.norbii@gmail.com>
2022-04-28 15:17:22 +0200

    remove old editor

--- Nork/Source/Editor2/Panels/All.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "AssetsPanel.h"
-#include "InspectorPanel.h"
-#include "LogPanel.h"
-#include "MainPanel.h"
-#include "ViewportPanel.h"
-#include "HierarchyPanel.h"
-#include "MeshEditor.h"
